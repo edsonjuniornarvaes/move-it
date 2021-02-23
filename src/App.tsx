06fbc46a31b7b96ad7edc83a485d9126c22841d1
edsonjuniornarvaes <edsonjunior.narvaes@gmail.com>
2021-02-22 23:09:50 -0300

    feat (Components): Implements components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Button } from './components/Button';
+
 function App() {
   return (
-    <h1>Hello NLW</h1>
+    <Button color="red">
+      Botão 1
+    </Button>
+    <Button color="blue">
+      Botão 2
+    </Button>
+    <Button color="green">
+      Botão 3
+    </Button>
   );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+interface ButtonProps {
+    color: string,
+    children: string,
+}
+
+export function Button(props: ButtonProps) {
+    return (
+        <button type="button" style={{ backgroundColor: props.color }}>
+            {props.children}
+        </button>
+    )
+}
