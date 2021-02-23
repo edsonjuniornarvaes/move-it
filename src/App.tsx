00d7dbcf8e00b73a267441ecd715285d41f6c519
edsonjuniornarvaes <edsonjunior.narvaes@gmail.com>
2021-02-23 00:48:09 -0300

    style (Global): Global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { Button } from './components/Button';
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
 
 function App() {
   return (
-    <Button color="red">
-      Botão 1
-    </Button>
-    <Button color="blue">
-      Botão 2
-    </Button>
-    <Button color="green">
-      Botão 3
-    </Button>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,17 @@
+export function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>8 xp</span>
+            <div>
+                <div style={{ width: '50%' }} />
+                <span 
+                    className="current-experience"
+                    style={{ left: '50%' }}
+                >
+                    300xp
+                </span>
+            </div>
+            <span>688 xp</span>
+        </header>
+    )
+}
